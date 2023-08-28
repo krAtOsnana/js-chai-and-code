22a22397bac1c0e30af52edac4fe405d73a8e5c7
kushagra_kumar <96875614+krAtOsnana@users.noreply.github.com>
2023-08-28 07:01:09 +0000

    type done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as a newer version
+/*
+alert(3+3) // we are using nodejs ,not browser
+*/
+let name="halwa";  //string
+let age=2;  //number
+ /*
+ premitive data type:
+
+ number=9  => range 2to the power -53 to +53
+ bigint =>to store larger number
+ string="chalo"
+ boolean=true/false
+ null=>typeof->object(stand alone value)
+ undefined=>typeof(->undefined)
+ symbol=>uniqueness
+ 
+ object:
+
+ */
+ console.log(typeof(age));
+ console.log(typeof(null));
+ console.log(typeof(undefined));
