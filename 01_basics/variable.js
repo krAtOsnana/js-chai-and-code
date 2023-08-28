48ac67c68400541896763b063e3ca839ce3a927f
kushagra_kumar <96875614+krAtOsnana@users.noreply.github.com>
2023-08-28 06:28:03 +0000

    console.table is not working

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,7 +1,12 @@
 const id=1969;
-var name="kushagra";
+let name="kushagra";
 let city="prayagraj"
+var college="uit";
+/* 
+we deont use var in js because its block scope and functional scope is not defined and var add itself to the window object
+*/
 address = "dhoomangang";
 console.table[{id,city,address}];
 console.log(name);
-console.log(city);
+console.log(city); 
+console.log(college)
