9cd5aa5e27b2bf7ddd71fc383ca002dd11966bc6
kushagra_kumar <96875614+krAtOsnana@users.noreply.github.com>
2023-09-09 06:02:35 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+if(true){ 
+    var a=1
+    const b=2
+    let c=3
+}
+// console.log(a); it print
+// console.log(b);  undefined
+// console.log(c);  undefined
+
+
+//another problem with var 
+var z=300
+
+if(true){
+    z=40
+}
+console.log(z);
