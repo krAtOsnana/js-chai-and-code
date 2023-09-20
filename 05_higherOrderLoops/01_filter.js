bfdbb523ec0407c19e7220ed331c1d8c42b044f4
Krat0s <kkushagra626@gmail.com>
2023-09-21 00:33:31 +0530

    nothing

--- 05_higherOrderLoops/01_filter.js
+++ 05_higherOrderLoops/01_filter.js
@@ -16,7 +16,16 @@ const nums=[1,2,3,4,5,6,7,8,9]
 const returnVal1=nums.filter((val)=>{
 //    var ans= val<6;
 //    return ans;
-    return val<6;
+    return val<6;       //HERE we have to explicitrly return a valur because we oopen a scope
 })
-const returnVal2=nums.filter((val)=>val<6)      //
-console.log(returnVal1);
+const returnVal2=nums.filter( (val) => val<6 )      //IMPLICITE RETURN
+//console.log(returnVal1);
+
+const newNums=[]
+ nums.forEach((val)=>{
+if(val>5){
+    newNums.push(val);
+    }
+ })
+
+ //console.log(newNums);  
