a1063bf9ed283b0e1900ced74c9de87f964f9401
Krat0s <kkushagra626@gmail.com>
2023-09-25 23:59:06 +0530

    promise half way

---
+++ 10_advance/promise.js
@@ -0,0 +1,36 @@
+// const promiseOne = new Promise(function(resolve,reject){
+//     //do a async task
+//     //DB call,cryptography,network
+//     setTimeout(function(){
+//         //console.log("async task done");
+//         resolve();
+
+//     },1000)
+// })
+
+// promiseOne.then(function(){
+//     //console.log("promise consumed");
+// })
+
+
+
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("async task 2 done");
+//         resolve()
+//     },5000)
+// }).then(function(){
+//     console.log("async task2 resolved");
+// })
+
+// const promiseThree= new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log("promise3 ");
+//         resolve({name:"kushagra",year:"3rd"})
+//     },1000)
+// })
+// promiseThree.then((studentINFO)=>{
+//     console.log(studentINFO);
+// })
+
+  
